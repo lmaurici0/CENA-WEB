d9f089ef4ae180957ccf5952d18fe6983d283ae5
Eric Luis <135241097+lmaurici0@users.noreply.github.com>
2024-03-11 21:11:43 -0300

    Fix: Fix salvarDados function to save data in DataBase

--- static/connection.js
+++ static/connection.js
@@ -1,67 +1,66 @@
+const {error} = require("console");
 const Sequelize = require("sequelize");
+const config = require("../config/config");
 
-const sequelize = new Sequelize("database_cena", "root", "cenapasslogin", {
-  host: "localhost",
-  dialect: "mysql",
-  logging: console.log
+const sequelize = new Sequelize(config.development);
+
+const User = sequelize.define('students', {
+        endereco_aluno: {
+                 type: Sequelize.STRING,
+                 allowNull: false
+         },
+        nasc_aluno: {
+                 type: Sequelize.DATE,
+                 allowNull: false
+         },
+        curso_aluno: {
+                 type: Sequelize.STRING,
+                 allowNull: false
+         },
+        foto_aluno: {
+                 type: Sequelize.STRING
+         },
+        nome_aluno: {
+                 type: Sequelize.STRING,
+                 allowNull: false,
+                 unique: true 
+         },
+        email_aluno: {
+                 type: Sequelize.STRING,
+                 allowNull: false,
+                 unique: true 
+         },
+        telefone_aluno: {
+                 type: Sequelize.STRING,
+                 allowNull: false
+         }
 });
 
-const Student = sequelize.define('student', {
-    endereco_aluno: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    nasc_aluno: {
-        type: Sequelize.DATE,
-        allowNull: false
-    },
-    curso_aluno: {
-        type: Sequelize.STRING,
-        allowNull: false
-    },
-    foto_aluno: {
-        type: Sequelize.STRING
-    },
-    nome_aluno: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true 
-    },
-    email_aluno: {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true 
-    },
-    telefone_aluno: {
-        type: Sequelize.STRING,
-        allowNull: false
-    }
-});
 
-async function salvarCadastro() {
+salvarDados = async () => {
     try {
-        const endereco = document.getElementById('end_std').value;
-        const dataNascimento = document.getElementById('bday_std').value;
-        const curso = document.getElementById('curso').value;
-        const foto = document.getElementById('inputFile').value;
-        const nome = document.getElementById('name_std').value;
-        const email = document.getElementById('email_std').value;
-        const telefone = document.getElementById('tel_std').value;
-
-        const novoAluno = await Student.create({
-           endereco_aluno: endereco,
-           nasc_aluno: dataNascimento,
-           curso_aluno: curso,
-           foto_aluno: foto,
-           nome_aluno: nome,
-           email_aluno: email,
-           telefone_aluno: telefone
-        });
-
-        console.log('Cadastro salvo com sucesso:', novoAluno);
+      await sequelize.authenticate();
+      console.log('Conexão bem-sucedida.');
+  
+      await sequelize.sync({ alter: true });
+      console.log('Modelos sincronizados com o banco de dados.');
+  
+      const user = await User.create({
+        endereco_aluno: 'rua white, 427',
+        nasc_aluno: '2008-01-02',
+        curso_aluno: 'Desenvolvimento de Sistemas',
+        nome_aluno: 'Eric Luis filho',
+        email_aluno: 'ericluisfilho@gmail.com',
+        telefone_aluno: '11983085255'
+        
+      });
+  
+      console.log('Usuário criado:', user.toJSON());
     } catch (error) {
-        console.error('Erro ao salvar o cadastro:', error);
+      console.error('Erro ao conectar ao banco de dados:', error);
+    } finally {
+      await sequelize.close();
     }
-}
+};
 
-salvarCadastro()
+salvarDados()
